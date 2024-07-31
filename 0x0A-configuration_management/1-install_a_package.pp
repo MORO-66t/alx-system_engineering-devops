029de1ea2ab5c941ba6f2d0cb12b6590c7220604
MORO-66t <166075118+MORO-66t@users.noreply.github.com>
2024-07-31 18:42:26 +0300

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
-# install puppet-lint -v 2.5.0
-
-exec { 'flask':
-  command => '/usr/bin/apt-get install flask -v 2.1.0',
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install flask==2.1.0',
+  path    => ['/usr/bin', '/usr/local/bin'],
+  unless  => '/usr/bin/pip3 show flask | grep "Version: 2.1.0"',
 }
