20af76dafc172543abe0436a52f441aa363a5e15
MORO-66t <166075118+MORO-66t@users.noreply.github.com>
2024-07-31 18:36:38 +0300

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,18 +1,5 @@
-# Ensure that pip3 is installed
-package { 'python3-pip':
-  ensure => installed,
-}
+# install puppet-lint -v 2.5.0
 
-# Install Flask version 2.1.0 using pip3
 exec { 'install_flask':
-  command => '/usr/bin/pip3 install flask==2.1.0',
-  path    => ['/usr/bin', '/usr/local/bin'],
-  unless  => '/usr/bin/pip3 show flask | grep "Version: 2.1.0"',
-}
-
-# Optionally, ensure Flask is installed correctly
-exec { 'verify_flask_version':
-  command => '/usr/bin/python3 -m flask --version',
-  path    => ['/usr/bin', '/usr/local/bin'],
-  onlyif  => '/usr/bin/pip3 show flask | grep "Version: 2.1.0"',
+  command => '/usr/bin/apt-get -y install flask -v 2.1.0',
 }
