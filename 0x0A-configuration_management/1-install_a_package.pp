a3caf549c520592b492f66c9c93befc534317781
MORO-66t <166075118+MORO-66t@users.noreply.github.com>
2024-07-31 18:46:45 +0300

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,16 @@
-# flaskd  
+# Ensure that pip3 is installed
+package { 'python3-pip':
+  ensure => installed,
+}
 
+# Install Werkzeug version 2.1.1
+exec { 'install_werkzeug':
+  command => '/usr/bin/pip3 install werkzeug==2.1.1',
+  path    => ['/usr/bin', '/usr/local/bin'],
+  unless  => '/usr/bin/pip3 show werkzeug | grep "Version: 2.1.1"',
+}
+
+# Install Flask version 2.1.0
 exec { 'install_flask':
   command => '/usr/bin/pip3 install flask==2.1.0',
   path    => ['/usr/bin', '/usr/local/bin'],
